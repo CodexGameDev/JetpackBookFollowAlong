8f6acfe326609c6295d5904b296fd9a7e6a3a282
andrew <andrewlaack1@gmail.com>
2022-08-23 00:18:30 -0500

    Added tux image to button and changed background using the painterResource feature

--- app/src/main/java/com/example/composebookfollowalong/MainActivity.kt
+++ app/src/main/java/com/example/composebookfollowalong/MainActivity.kt
@@ -1,44 +1,83 @@
 
 package com.example.composebookfollowalong
 
+import android.graphics.ColorFilter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composebookfollowalong.ui.theme.ComposeBookFollowAlongTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeBookFollowAlongTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+               screenShow()
             }
         }
-    }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun screenShow()
+{
+    imageButton()
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeBookFollowAlongTheme {
-        Greeting("Android")
+fun imageButton()
+{
+
+    //Full screen tux button with text
+
+    val random : Double = Random.nextDouble()
+
+    Button(onClick = { /*TODO*/ },
+        modifier = Modifier,
+        colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(id = R.color.teal_200) )
+
+    )
+        {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+            ,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+
+
+        ) {
+        Image( painter = painterResource(id = R.drawable.tux), "Tux")
+        Text(text = "This is Tux Click Him!")
+        }
     }
+
+
+}
+
+@Composable
+@Preview (showBackground = true , showSystemUi = true)
+
+fun preview()
+{
+    screenShow()
 }
