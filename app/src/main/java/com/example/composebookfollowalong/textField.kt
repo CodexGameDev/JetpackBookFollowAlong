4097cc37e5de3a12df88a30114713e98068ccb69
andrew <andrewlaack1@gmail.com>
2022-08-31 00:25:35 -0500

    Added new composable that has a textfield and returns the value in the field to the other composable which will then use that to display the text when if the text input is less than 4 characters long.

---
+++ app/src/main/java/com/example/composebookfollowalong/textField.kt
@@ -0,0 +1,53 @@
+package com.example.composebookfollowalong
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composebookfollowalong.ui.theme.ComposeBookFollowAlongTheme
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+
+    var inputTxt = remember {
+        mutableStateOf("")
+    }
+    Column(modifier = Modifier.fillMaxSize(.5f)) {
+        TextInput()
+        {
+            if(it.length < 5) {
+                inputTxt.value = it
+            }
+
+        }
+
+        Text(text = inputTxt.value)
+    }
+
+}
+
+
+@Composable
+fun TextInput(returnVal : (String) -> Unit)
+{
+    var txtValue = remember {
+        mutableStateOf("")
+    }
+    TextField(value = txtValue.value, onValueChange =
+    {
+        txtValue.value = it
+        returnVal(it)
+    } )
+}
