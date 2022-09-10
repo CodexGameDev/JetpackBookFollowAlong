d0ecae06e6d9f2317d1423493cc8249e0617be1e
andrew <andrewlaack1@gmail.com>
2022-09-10 15:28:30 -0500

    Messing around with column, rotation, and dynamic composable colors.

--- app/src/main/java/com/example/composebookfollowalong/AlignmentAndArrangment.kt
+++ app/src/main/java/com/example/composebookfollowalong/AlignmentAndArrangment.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.slideIn
 import androidx.compose.animation.slideOut
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
@@ -15,12 +16,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.graphics.rotationMatrix
 import java.security.cert.TrustAnchor
+import kotlin.random.Random
+
 //ToDo Create column test for the alignment and arrangment bc I am a bit confused about it.
 @Composable
 fun RowTest()
@@ -50,10 +58,11 @@ fun RowTest()
                           painter = painterResource(id = R.drawable.tux),
                           contentDescription = "Tux",
                           contentScale = ContentScale.Fit,
-                        modifier = Modifier.clickable {
-                            rotation.value += 90f
+                        modifier = Modifier
+                            .clickable {
+                                rotation.value += 90f
 
-                        }
+                            }
                             .rotate(rotation.value)
                       )
                   }
@@ -65,7 +74,72 @@ fun RowTest()
 }
 
 
+@Composable
+fun ColumnTest()
+{
+    var rotation1 = remember {
+        mutableStateOf(0f)
+    }
+    var rotation2 = remember {
+        mutableStateOf(0f)
+    }
+    var rotation3 = remember {
+        mutableStateOf(0f)
+    }
+    var color : Color = colorResource(R.color.teal)
+    if(rotation1.value > 200f)
+    {
+        color = colorResource(R.color.white)
+    }
 
+
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color)
+
+
+
+
+
+    )
+    {
+
+
+            Text("Text 1", modifier = Modifier.rotate(rotation3.value))
+
+
+            Text("Text 2", modifier = Modifier.rotate(rotation2.value))
+
+
+           Box(modifier = Modifier.fillMaxSize(.1f)){
+               Image(painter = painterResource(id = R.drawable.tux), contentDescription = "Tux", contentScale = ContentScale.Fit, modifier = Modifier
+                   .clickable
+                   {
+
+                       rotation1.value = Random.nextFloat() * 360
+                       rotation2.value = Random.nextFloat() * 360
+                       rotation3.value = Random.nextFloat() * 360
+
+
+                   }
+                   .rotate(rotation1.value)
+               )
+
+    }
+
+
+            if(rotation2.value > 200f)
+            {
+
+                Text(text = "200+ Achieved!!!!!!!!!!!!!!!" , fontSize = 30.sp)
+            }
+
+    }
+}
 
 
 
@@ -73,5 +147,5 @@ fun RowTest()
 @Preview(showBackground = true, showSystemUi = true)
 fun Preview()
 {
-    RowTest()
+    ColumnTest()
 }
