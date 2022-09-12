a815135c59fad5316b2b1fa233cd3d2947fba489
andrew <andrewlaack1@gmail.com>
2022-09-12 12:44:23 -0500

    Import Purge

--- app/src/main/java/com/example/composebookfollowalong/MainActivity.kt
+++ app/src/main/java/com/example/composebookfollowalong/MainActivity.kt
@@ -1,21 +1,8 @@
 package com.example.composebookfollowalong
-import BoxMod
-
 import android.os.Bundle
-
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.ScrollableState
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Text
-
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-
 import androidx.compose.ui.tooling.preview.Preview
 
 
