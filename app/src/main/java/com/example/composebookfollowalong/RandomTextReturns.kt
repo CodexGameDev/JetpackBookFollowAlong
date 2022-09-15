688ed6f8e9e8299ada078af2c270dcbbc0e272a8
andrew <andrewlaack1@gmail.com>
2022-09-15 12:28:46 -0500

    Returns random data.

---
+++ app/src/main/java/com/example/composebookfollowalong/RandomTextReturns.kt
@@ -0,0 +1,8 @@
+package com.example.composebookfollowalong
+fun
+RandomTextLong() : String
+{
+    return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit scelerisque mauris pellentesque pulvinar. Placerat in egestas erat imperdiet sed euismod nisi porta. Faucibus a pellentesque sit amet. Ultricies mi eget mauris pharetra et ultrices. Facilisi morbi tempus iaculis urna. Diam maecenas sed enim ut sem. Donec ultrices tincidunt arcu non sodales neque sodales. Eu augue ut lectus arcu bibendum at varius. Adipiscing commodo elit at imperdiet dui accumsan. Vitae congue eu consequat ac felis donec et odio.\n" +
+            "\n" +
+            "Morbi tristique senectus et netus et malesuada fames ac. Velit scelerisque in dictum non consectetur a erat. At lectus urna duis convallis convallis tellus id interdum. Convallis aenean et tortor at risus viverra adipiscing at. Feugiat in fermentum posuere urna nec. Donec ultrices tincidunt arcu non sodales. Lobortis mattis aliquam faucibus purus in massa tempor nec feugiat. Volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris. Viverra adipiscing at in tellus integer feugiat. Augue lacus viverra vitae congue eu consequat ac felis donec. Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque."
+}
