954328ea1eefce7ed8c7385b77f727349f8d2011
andrew <andrewlaack1@gmail.com>
2022-09-29 22:26:14 -0500

    Created data class to store article contents then made a composable to display it using material theme typography and local alpha.

--- app/src/main/java/com/example/composebookfollowalong/FadedTextTesting.kt
+++ app/src/main/java/com/example/composebookfollowalong/FadedTextTesting.kt
@@ -1,14 +1,14 @@
 package com.example.composebookfollowalong
 
+import android.text.Layout
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.LocalContentAlpha
-import androidx.compose.material.LocalContentColor
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import org.intellij.lang.annotations.JdkConstants
 
 
 @Composable
@@ -39,4 +39,28 @@ fun FadedPyramid(stringsList : ArrayList<String>)
             counter += 1
         }
     }
+}
+
+
+@Composable
+fun FadedArticle(textStuff : Article)
+{
+
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = textStuff.Alignment  )
+    {
+        Text(text = textStuff.heading, style = MaterialTheme.typography.h6)
+
+        CompositionLocalProvider(LocalContentAlpha provides .9f) {
+            Text(text = textStuff.body)
+        }
+
+    }
+
+}
+
+data class Article(var heading : String , var body : String, var Alignment  : Alignment.Horizontal )
+
+fun defaultArticle() : Article
+{
+    return Article("Hurricane Ian live updates: Biden warns of 'substantial loss of life' as storm strengthens back into a hurricane", "President Joe Biden said Hurricane Ian may be responsible for \"substantial loss of life\" and could end up being the deadliest storm in Florida history.\n" + "\n" + "By Thursday night, the death toll had climbed to 12, although the state has refused to officially comment on deaths. " , Alignment.Start)
 }
