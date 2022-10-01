e9f97b56aa048034dfda633944e814ba3b908a0b
andrew <andrewlaack1@gmail.com>
2022-09-30 22:18:36 -0500

    Configured constructor to pass in text and alpha of text to display in a column.

--- app/src/main/java/com/example/composebookfollowalong/MainActivity.kt
+++ app/src/main/java/com/example/composebookfollowalong/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.composebookfollowalong
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 
@@ -29,8 +33,18 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainPreview()
 {
-    var article = defaultArticle()
-    FadedArticle(textStuff = article)
+    Column( horizontalAlignment = Alignment.Start) {
+
+        var fade = 1f
+        while(fade > 0f) {
+            //This weird stuff is used to counteract the effect of float division inaccuracy
+            fadedText2(text = "Top Row " + ((fade + .001) * 100).toInt() + "%" , fadePercent = fade)
+            fade -= .1f
+
+        }
+
+
+    }
 }
 
 
